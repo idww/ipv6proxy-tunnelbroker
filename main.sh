2eddb780ee8fc314ef6996e4042f11041366257f
avcvv <39009179+avcvv@users.noreply.github.com>
2022-06-05 01:24:20 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,8 +7,61 @@ else
     exit 1
 fi
 
+#### Update system
+echo "● Updating packages and installing dependencies"
+apt-get update
+apt-get -y install gcc g++ make bc pwgen git
+
+#### Tuning sysctl
+echo "● Setting up /etc/sysctl.conf"
+cat >>/etc/sysctl.conf <<END
+net.ipv6.conf.eth0.proxy_ndp=1
+net.ipv6.conf.all.proxy_ndp=1
+net.ipv6.conf.default.forwarding=1
+net.ipv6.conf.all.forwarding=1
+net.ipv6.ip_nonlocal_bind=1
+net.ipv4.ip_local_port_range=1024 64000
+net.ipv6.route.max_size=409600
+net.ipv4.tcp_max_syn_backlog=4096
+net.ipv6.neigh.default.gc_thresh3=102400
+kernel.threads-max=1200000
+kernel.max_map_count=6000000
+vm.max_map_count=6000000
+kernel.pid_max=2000000
+END
+
+#### Tuning
+echo "● Setting up /etc/systemd/logind.conf"
+echo "UserTasksMax=1000000" >>/etc/systemd/logind.conf
+
+#### Tuning file limits
+echo '* hard nofile 999999' >> /etc/security/limits.conf
+echo '* soft nofile 999999' >> /etc/security/limits.conf
+echo 'root hard nofile 1048576' >> /etc/security/limits.conf
+echo 'root soft nofile 1048576' >> /etc/security/limits.conf
+
+
+#### Tuning
+echo "● Setting up /etc/systemd/system.conf"
+cat >>/etc/systemd/system.conf <<END
+UserTasksMax=1000000
+DefaultMemoryAccounting=no
+DefaultTasksAccounting=no
+DefaultTasksMax=1000000
+UserTasksMax=1000000
+END
+
+#### Install 3proxy
+echo "● Setting up 3proxy"
+cd ~
+git clone https://github.com/z3APA3A/3proxy.git
+cd 3proxy/
+touch ~/3proxy/src/define.txt
+echo "#define ANONYMOUS 1" > ~/3proxy/src/define.txt
+make -f Makefile.Linux
+
 ####
-echo "↓ Routed IPv6 Prefix (*:*:*::/*):"
+echo "↓ Routed IPv6 Prefix (*:*:*::/*): (enter full prefix eg: 2001:470:7ac7::/48 or 2001:470:1f15:3c4::/64)"
 read PROXY_NETWORK
 
 if [[ $PROXY_NETWORK == *"::/48"* ]]; then
@@ -22,7 +75,7 @@ fi
 echo "● Selected: $PROXY_NETWORK"
 
 ####
-echo "↓ IPv4 endpoint of your Tunnel Server:"
+echo "↓ IPv4 endpoint of your Tunnel Server: (get it in tunnelbroker Server IPv4 Address:"
 read TUNNEL_IPV4_ADDR
 if [[ ! "$TUNNEL_IPV4_ADDR" ]]; then
     echo "IPv4 endpoint can't be emty"
@@ -30,24 +83,111 @@ if [[ ! "$TUNNEL_IPV4_ADDR" ]]; then
 fi
 echo "● Selected: $TUNNEL_IPV4_ADDR"
 
-####
-echo "↓ Proxies login (can be blank):"
+#### Authorization method
+echo "↓ Proxies authorisation mode 0 or 1 or 2 : (0 = log;pass / 1 = ip / 2 = no auth)"
+read PROXY_AUTHORISATION
+
+if [[ $PROXY_AUTHORISATION == *"0"* ]]; then
+    PROXY_AUTH_TYPE="auth strong"
+elif [[ $PROXY_AUTHORISATION == *"1"* ]]; then
+    PROXY_AUTH_TYPE="auth iponly"
+elif [[ $PROXY_AUTHORISATION == *"2"* ]]; then
+    PROXY_AUTH_TYPE="auth none"
+else
+    echo "● Unsupported auth format: $PROXY_AUTHORISATION"
+    exit 1
+fi
+echo "● Selected auth method: $PROXY_AUTHORISATION"
+
+#### Generate log;pass
+gen_logpass() {
+echo "↓ Set proxies login & password:"
 read PROXY_LOGIN
 
-if [[ "$PROXY_LOGIN" ]]; then
+if [[ -n "$PROXY_LOGIN" ]]; then
     echo "● Selected: $PROXY_LOGIN"
 
     echo "↓ Proxies password:"
     read PROXY_PASS
     echo "● Selected: $PROXY_PASS"
+else
+    echo "● Login can't be empty"
+    exit 1
 
 fi
+}
+#gen_logpass
+
+#### Generate ip
+gen_auth_ip() {
+echo "↓ Enter your authorized ip (eg: 1.2.3.4.5):"
+read PROXY_AUTH_IP
+
+if [[ -n "$PROXY_AUTH_IP" ]]; then
+    echo "● Selected: $PROXY_AUTH_IP"
+else
+    echo "field can't be empty"
+    exit 1
+fi
+}
+#gen_auth_ip
+
+gen_conf() {
+echo "↓ Writing main 3proxy config"
+cat >~/3proxy/3proxy.cfg <<END
+#!/bin/bash
+daemon
+maxconn 10000
+nserver 1.1.1.1
+nserver [2606:4700:4700::1111]
+nserver [2606:4700:4700::1001]
+nserver [2001:4860:4860::8888]
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6000
+flush
+END
+}
+
+gen_conf
+
+#### Select authorization method
+echo "↓ Configuring proxy authorisation"
+if [[ $PROXY_AUTHORISATION == *"0"* ]]; then
+    gen_logpass
+    cat >>~/3proxy/3proxy.cfg <<END
+auth strong
+users ${PROXY_LOGIN}:CL:${PROXY_PASS}
+allow ${PROXY_LOGIN}
+END
+    echo "Chosen auth type: ($PROXY_AUTH_TYPE) proxy credentials:  Login = $PROXY_LOGIN Pass = $PROXY_PASS"
+elif [[ $PROXY_AUTHORISATION == *"1"* ]]; then
+    gen_auth_ip
+    cat >>~/3proxy/3proxy.cfg <<END
+auth iponly
+allow * ${PROXY_AUTH_IP}
+END
+    echo "Chosen auth type: ($PROXY_AUTH_TYPE) && authorized ip = $PROXY_AUTH_IP"
+elif [[ $PROXY_AUTHORISATION == *"2"* ]]; then
+    cat >>~/3proxy/3proxy.cfg <<END
+$PROXY_AUTH_TYPE
+END
+    echo "Chosen auth type: ($PROXY_AUTH_TYPE) anyone can access to proxy!"
+else
+    echo "● Unsupported auth format: $PROXY_AUTHORISATION"
+    exit 1
+fi
+#echo "● Selected: $PROXY_AUTHORISATION"
+####
+
 
 ####
-echo "↓ Port numbering start (default 1500):"
+echo "↓ Port numbering start (default 30000):"
 read PROXY_START_PORT
 if [[ ! "$PROXY_START_PORT" ]]; then
-    PROXY_START_PORT=1500
+    PROXY_START_PORT=30000
 fi
 echo "● Selected: $PROXY_START_PORT"
 
@@ -74,44 +214,7 @@ echo "● Selected: Network Mask=$PROXY_NET_MASK"
 HOST_IPV4_ADDR=$(hostname -I | awk '{print $1}')
 echo "● Selected: Host IPv4 address=$HOST_IPV4_ADDR"
 
-####
-echo "● Updating packages and installing dependencies"
-apt-get update
-apt-get -y install gcc g++ make bc pwgen git
-
-####
-echo "● Setting up /etc/sysctl.conf"
-cat >>/etc/sysctl.conf <<END
-net.ipv6.conf.eth0.proxy_ndp=1
-net.ipv6.conf.all.proxy_ndp=1
-net.ipv6.conf.default.forwarding=1
-net.ipv6.conf.all.forwarding=1
-net.ipv6.ip_nonlocal_bind=1
-net.ipv4.ip_local_port_range=1024 64000
-net.ipv6.route.max_size=409600
-net.ipv4.tcp_max_syn_backlog=4096
-net.ipv6.neigh.default.gc_thresh3=102400
-kernel.threads-max=1200000
-kernel.max_map_count=6000000
-vm.max_map_count=6000000
-kernel.pid_max=2000000
-END
-
-####
-echo "● Setting up /etc/systemd/logind.conf"
-echo "UserTasksMax=1000000" >>/etc/systemd/logind.conf
-
-####
-echo "● Setting up /etc/systemd/system.conf"
-cat >>/etc/systemd/system.conf <<END
-UserTasksMax=1000000
-DefaultMemoryAccounting=no
-DefaultTasksAccounting=no
-DefaultTasksMax=1000000
-UserTasksMax=1000000
-END
-
-####
+#### Install ndppd
 echo "● Setting up ndppd"
 cd ~
 git clone https://github.com/DanielAdolfsson/ndppd.git
@@ -129,45 +232,6 @@ proxy he-ipv6 {
    }
 }
 END
-
-####
-echo "● Setting up 3proxy"
-cd ~
-wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/0.8.13.tar.gz
-tar xzf 0.8.13.tar.gz
-mv ~/3proxy-0.8.13 ~/3proxy
-rm 0.8.13.tar.gz
-cd ~/3proxy
-chmod +x src/
-touch src/define.txt
-echo "#define ANONYMOUS 1" >src/define.txt
-sed -i '31r src/define.txt' src/proxy.h
-make -f Makefile.Linux
-cat >~/3proxy/3proxy.cfg <<END
-#!/bin/bash
-
-daemon
-maxconn 10000
-nserver 1.1.1.1
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-stacksize 6000
-flush
-END
-
-if [[ "$PROXY_LOGIN" ]]; then
-    cat >>~/3proxy/3proxy.cfg <<END
-auth strong
-users ${PROXY_LOGIN}:CL:${PROXY_PASS}
-allow ${PROXY_LOGIN}
-END
-else
-    cat >>~/3proxy/3proxy.cfg <<END
-auth none
-END
-fi
 
 ####
 echo "● Generating $PROXY_COUNT IPv6 addresses"
@@ -201,7 +265,14 @@ for e in $(cat ~/ip.list); do
     let "CURRENT_PROXY_PORT+=1"
 done
 
-####
+#### Write NET prefix & ip count  to files
+echo $PROXY_NETWORK > v_network.txt
+echo $PROXY_COUNT > v_count.txt
+echo $PROXY_NET_MASK > v_netmask.txt
+echo $PROXY_AUTHORISATION > v_authmode.txt
+echo $PROXY_AUTH_IP > v_authip.txt
+
+####  Configure autorun
 echo "● Setting up /etc/rc.local"
 cat >/etc/rc.local <<END
 #!/bin/bash
@@ -220,7 +291,7 @@ sleep 5
 /sbin/ip -6 route add 2000::/3 dev he-ipv6
 ~/ndppd/ndppd -d -c ~/ndppd/ndppd.conf
 sleep 2
-~/3proxy/src/3proxy ~/3proxy/3proxy.cfg
+~/3proxy/bin/3proxy ~/3proxy/3proxy.cfg
 exit 0
 
 END
